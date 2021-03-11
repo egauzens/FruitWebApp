b42a34922e40b4085bf0c0c3f19e07069bab2182
Eric Gauzens <ericgauzens@hotmail.com>
2021-03-10 21:27:15 -0500

    Added shop button

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
 	return (
 		<div>
 			<h1>Welcome to F.R.U.I.T.</h1>
-			<h3>This website is currently under development...</h3>
+			<h3>
+				This website is currently under development, but in the meantime
+				feel free to visit our shop!
+			</h3>
+			<Button href="https://urban-greenworks-inc.square.site/">
+				SHOP
+			</Button>
 		</div>
 	);
 }

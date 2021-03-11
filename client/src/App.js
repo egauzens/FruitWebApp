d246b2344501894c1f7fc75689d060e22700edfe
Eric Gauzens <ericgauzens@hotmail.com>
2021-03-11 10:58:06 -0500

    Added background image and updated text color to gradient

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { Button } from "react-bootstrap";
 
 function App() {
 	return (
-		<div>
-			<h1>Welcome to F.R.U.I.T.</h1>
-			<h3>
-				This website is currently under development, but in the meantime
-				feel free to visit our shop!
-			</h3>
-			<Button href="https://urban-greenworks-inc.square.site/">
-				SHOP
-			</Button>
+		<div className="backgroundContainer">
+			<div className="centerVertical">
+				<h1>Welcome to F.R.U.I.T.</h1>
+				<h3>
+					This website is currently under development, but in the
+					meantime feel free to visit our shop!
+				</h3>
+				<Button href="https://urban-greenworks-inc.square.site/">
+					SHOP
+				</Button>
+			</div>
 		</div>
 	);
 }
